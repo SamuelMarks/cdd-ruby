51551fa1b90f7673121532195fab9b40ce5bf6a0
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2026-03-07 12:27:57 +1300

    Fix CI failures

---
+++ src/client_sdk/parse.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+module Cdd
+# Client SDK module
+  module ClientSdk
+    # SDK Parser
+    class Parser
+      # Parses SDK
+      # @param tokens [Array] tokens
+      # @param ir [Object] IR
+      def self.parse(tokens, ir)
+        current_def = nil
+        tokens.each_with_index do |token, i|
+          if token[1] == :on_kw && token[2] == "def"
+            current_def = true
+          elsif token[1] == :on_ident && current_def
+            op_id = token[2]
+            ir.openapi_spec["paths"] ||= {}
+            ir.openapi_spec["paths"]["/#{op_id}"] ||= {
+              "get" => { "operationId" => op_id }
+            }
+            current_def = nil
+          end
+        end
+      end
+      # Supported keys
+      # @return [Array]
+      def self._supported_keys
+        ["jsonSchemaDialect", "termsOfService", "contact", "license", "name", "url", "email", "identifier", "variables", "enum", "default", "description", "schemas", "responses", "parameters", "examples", "requestBodies", "headers", "securitySchemes", "links", "callbacks", "pathItems", "mediaTypes", "summary", "get", "put", "post", "delete", "options", "head", "patch", "trace", "query", "additionalOperations", "operationId", "requestBody", "deprecated", "security", "in", "required", "allowEmptyValue", "example", "style", "explode", "allowReserved", "schema", "content", "itemSchema", "encoding", "prefixEncoding", "itemEncoding", "contentType", "200", "expression", "dataValue", "serializedValue", "externalValue", "value", "operationRef", "server", "parent", "kind", "discriminator", "xml", "propertyName", "mapping", "defaultMapping", "nodeType", "namespace", "prefix", "attribute", "wrapped", "type", "scheme", "bearerFormat", "flows", "openIdConnectUrl", "oauth2MetadataUrl", "implicit", "password", "clientCredentials", "authorizationCode", "deviceAuthorization", "authorizationUrl", "deviceAuthorizationUrl", "tokenUrl", "refreshUrl", "scopes"]
+      end
+    end
+  end
+end
+
