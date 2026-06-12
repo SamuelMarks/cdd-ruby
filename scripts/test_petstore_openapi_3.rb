71e76c1aa254b66df84a1b1de3fc476f1ae5f15c
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2026-06-12 20:01:11 +1200

    Test both Swagger and OpenAPI on precommit

--- scripts/test_petstore_openapi_3.rb
+++ scripts/test_petstore_openapi_3.rb
@@ -2,6 +2,8 @@
 # frozen_string_literal: true
 
 require 'fileutils'
+require 'net/http'
+require 'uri'
 
 dir = File.expand_path('../../cdd-ruby-client-v3', __dir__)
 project_root = File.expand_path('..', __dir__)
@@ -16,12 +18,85 @@ Dir.chdir(project_root) do
   end
 end
 
-if Dir.exist?(dir)
-  Dir.chdir(dir) do
-    system('bundle install')
-    # Ignore rspec failure due to missing mock server (as in original bash script)
-    system('bundle exec rspec')
+srand(Process.pid)
+port = rand(8000..8999)
+jvm_container = "cdd_petstore_jvm_v3_#{Time.now.to_i}"
+non_jvm_container = "cdd_petstore_non_jvm_v3_#{Time.now.to_i}"
+
+puts 'Attempting to start JVM container...'
+system("docker run --rm -d -p #{port}:8080 --name #{jvm_container} openapitools/openapi-petstore:latest")
+
+jvm_ready = false
+base_path = '/api/v3'
+30.times do
+  begin
+    res = Net::HTTP.get_response(URI("http://localhost:#{port}#{base_path}/pet/findByStatus?status=available"))
+    if res.is_a?(Net::HTTPSuccess) || res.code == '404'
+      jvm_ready = true
+      break
+    end
+  rescue StandardError
+    # ignore
+  end
+  sleep 2
+end
+
+container_name = jvm_container
+
+unless jvm_ready
+  puts 'JVM container failed to respond. Falling back to non-JVM container...'
+  system("docker stop #{jvm_container} 2>/dev/null")
+  system("docker run --rm -d -p #{port}:8080 --name #{non_jvm_container} swaggerapi/petstore")
+  container_name = non_jvm_container
+  base_path = '/api'
+
+  non_jvm_ready = false
+  30.times do
+    begin
+      res = Net::HTTP.get_response(URI("http://localhost:#{port}#{base_path}/pet/findByStatus?status=available"))
+      if res.is_a?(Net::HTTPSuccess)
+        non_jvm_ready = true
+        break
+      end
+    rescue StandardError
+      # ignore
+    end
+    sleep 2
+  end
+
+  unless non_jvm_ready
+    puts 'Non-JVM container also failed.'
+    system("docker stop #{container_name} 2>/dev/null")
+    exit 1
   end
 end
 
+if Dir.exist?(dir)
+  Dir.chdir(dir) do
+    FileUtils.mkdir_p('spec')
+    File.write('spec/client_spec.rb', <<~SPEC)
+      require_relative '../lib/client'
+
+      RSpec.describe ClientSdk do
+        let(:client) { ClientSdk.new('http://localhost:#{port}#{base_path}') }
+
+        it 'fetches pets by status' do
+          response = client.findPetsByStatus(status: 'available')
+          expect(response).to be_an(Array)
+        end
+      end
+    SPEC
+
+    File.open('Gemfile', 'a') { |f| f.puts "gem 'rspec'" } unless File.read('Gemfile').include?('rspec')
+
+    system('bundle install')
+    unless system('bundle exec rspec')
+      puts 'RSpec failed!'
+      system("docker stop #{container_name} 2>/dev/null")
+      exit 1
+    end
+  end
+end
+
+system("docker stop #{container_name} 2>/dev/null")
 FileUtils.rm_rf(dir)
